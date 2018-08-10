22753ee12d798a7ebd214b17fa2abd4d19568fae
xiaodezhang <zhangxiaode0@gmail.com>
2018-08-10 13:42:45 +0800

    OspClient with msc

---
+++ osptest/prj_linux/project.sh
@@ -0,0 +1,7 @@
+vim \
+../source/client.cpp \
+../source/demogui.cpp \
+../../common/include/osp.h \
+../include/client.h \
+../include/commondemo.h
+../include/demogui.h
---
+++ osptest/prj_vs2010/project.bat
@@ -0,0 +1,1 @@
+gvim ../source/client.cpp ../source/demogui.cpp ../source/main.cpp ../../common/include/osp.h ../include/client.h ../include/commondemo.h ../include/demogui.h
